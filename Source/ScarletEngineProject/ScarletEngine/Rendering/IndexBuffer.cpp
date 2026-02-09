db28268ee692bdb04db0e3d700042592ff39db42
Hidd3n47 <122316898+Hidd3n-47@users.noreply.github.com>
2026-02-09 15:34:38 +0000

    25: IndexBuffer A class representing the index buffer. Index buffer will be used to handle the commands relating to the indices and the commands for uploading to the GPU.

---
+++ Source/ScarletEngineProject/ScarletEngine/Rendering/IndexBuffer.cpp
@@ -0,0 +1,45 @@
+#include "ScarletEnginePch.h"
+#include "IndexBuffer.h"
+
+#include <glew/glew.h>
+
+namespace Scarlet
+{
+
+IndexBuffer::IndexBuffer(const uint32 count)
+{
+    glCreateBuffers(1, &mId);
+    Bind();
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, static_cast<long long>(count * sizeof(uint32)), nullptr, GL_DYNAMIC_DRAW);
+}
+
+IndexBuffer::IndexBuffer(const uint32* indices, const uint32 count)
+{
+    glCreateBuffers(1, &mId);
+    Bind();
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, static_cast<long long>(count * sizeof(uint32)), indices, GL_DYNAMIC_DRAW);
+}
+
+IndexBuffer::~IndexBuffer()
+{
+    glDeleteBuffers(1, &mId);
+}
+
+void IndexBuffer::Bind() const
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mId);
+}
+
+void IndexBuffer::Unbind()
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void IndexBuffer::SetData(const uint32* indices, const uint32 count) const
+{
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mId);
+
+    glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, static_cast<long long>(count * sizeof(uint32)), indices);
+}
+
+} // Namespace Scarlet.
---
+++ Source/ScarletEngineProject/ScarletEngine/Rendering/IndexBuffer.h
@@ -0,0 +1,50 @@
+#pragma once
+
+namespace Scarlet
+{
+
+/**
+ * @class IndexBuffer: A class responsible for uploading the indices to the GPU and storing a unique \\n
+ * identifier for the index buffer.
+ */
+class IndexBuffer
+{
+public:
+    /**
+     * @brief Construct an index buffer with the number of indices but pass in the indices later.
+     * @param count The number of indices in the buffer.
+     */
+    IndexBuffer(const uint32 count);
+    /**
+     * @brief Construct an index buffer with the indices and the number of indices.
+     * @param indices A pointer to the indices buffer.
+     * @param count The number of indices in the buffer.
+     */
+    IndexBuffer(const uint32* indices, const uint32 count);
+    ~IndexBuffer();
+
+    IndexBuffer(const IndexBuffer&)            = delete;
+    IndexBuffer(IndexBuffer&&)                 = delete;
+    IndexBuffer& operator=(IndexBuffer&&)      = delete;
+    IndexBuffer& operator=(const IndexBuffer&) = delete;
+
+    /**
+     * @brief Bind the buffer so the graphics pipeline uses this index buffer.
+     */
+    void Bind() const;
+    /**
+     * @brief Unbind the currently bound index buffer.
+     */
+    static void Unbind();
+
+    /**
+     * @brief Set the data of the buffer. This will inform the graphics pipeline to update the buffer.
+     * @param indices A pointer to the buffer of indices.
+     * @param count The number of indices.
+     */
+    void SetData(const uint32* indices, const uint32 count) const;
+private:
+    uint32 mId;
+};
+
+} // Namespace Scarlet.
